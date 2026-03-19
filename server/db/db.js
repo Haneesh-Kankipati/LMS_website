4e06707b1562aea4d98b9db2249a270f021df3ea
Haneesh-Kankipati <haneeshkankipati@gmail.com>
2026-03-20 02:54:38 +0530

    please

--- server/db/db.js
+++ server/db/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async ()=>{
     try {
+        console.log("Connecting to database");
         await mongoose.connect(process.env.MONGODB_URL)
-        console.log("ENV CHECK:", process.env.MONGODB_URL);
+        
     } catch (error) {
         console.log(error)
     }
