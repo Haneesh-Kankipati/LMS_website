fd22ff38eea447b71d495f4bd2b10eb488b84fcb
Haneesh-Kankipati <haneeshkankipati@gmail.com>
2026-03-20 20:00:01 +0530

    i think i got it

--- server/index.js
+++ server/index.js
@@ -43,6 +43,6 @@ app.use('/api/settings',settingsRouter)
 app.use('/api/dashboard',dashboardRouter)
 app.use('/api/gallery',galleryRouter)
 app.use('/api/feestructure',feeStructureRouter)
-app.listen(process.env.PORT,()=>{
+/*app.listen(process.env.PORT,()=>{
     console.log(`Server running on port ${process.env.PORT}`)
-})
+})*/
